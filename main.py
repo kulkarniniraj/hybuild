e62314d29fb7352f4a8a0feef63852a19f2e39cd
Niraj Kulkarni <niraj.kulkarni@druva.com>
2024-04-12 09:57:16 +0530

    1. quote rule working as expected 2. rule with string and list recipes work 3. variable interpolation in recipe works 4. xv6 makefile with everything converted to quote rules and few things to actual rules

---
+++ main.py
@@ -0,0 +1,77 @@
+import hy 
+import sys
+from icecream import ic
+from pathlib import Path
+import yaml
+import os
+import fire
+
+STATE = {}
+
+def _quote_rule(string):
+    with open('Makefile', 'a') as f:
+        f.write(string)
+        f.write('\n')
+
+def QUOTE_RULE(string):
+    return _quote_rule(string)
+
+def _rule(target, deps, recipes):
+    def _process_token(tok: str):
+        if tok.startswith('$'):
+            val = _get(tok.lstrip('$'))
+            return val
+        else:
+            return tok
+
+    def _process_one_recipe(rule):
+        if isinstance(rule, str):
+            return rule
+        else:
+            return ' '.join([_process_token(tok) for tok in rule])
+    
+    lines = []
+    lines.append(f'{target}: {" ".join(deps)}')
+    for rec in recipes:
+        lines.append(f'\t{_process_one_recipe(rec)}')
+    with open('Makefile', 'a') as f:
+        for line in lines:
+            f.write(f'{line}\n')
+        f.write('\n')
+
+def RULE(target, deps = [], recipes = []):
+    return _rule(target, deps, recipes)
+
+def _set(var, val):
+    STATE[var] = val
+
+def SET(var, val):
+    return _set(var, val)
+
+def _get(var):
+    return STATE.get(var, None)
+
+def GET(var):
+    return _get(var)
+
+def _is_set(var):
+    return var in STATE
+
+def IS_SET(var):
+    return _is_set(var)
+
+def IS_NOT_SET(var):
+    return not _is_set(var)
+
+def main(spec_file):
+    target = Path(spec_file)
+    with open('Makefile', 'w') as f:
+        pass
+
+    with open(target) as f:
+        stream = hy.read_many(f)
+        for expr in stream:
+            hy.eval(expr)
+
+if __name__ == '__main__':
+    fire.Fire(main)
